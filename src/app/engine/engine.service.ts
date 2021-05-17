32ffed376ed69c43e033b9de6e411ad98012fc23
Mateusz Ziobrowski <matrix.ziobrowski@gmail.com>
2021-05-17 17:12:56 +0100

    ThreeJS implemented

---
+++ src/app/engine/engine.service.ts
@@ -0,0 +1,15 @@
+// import
+import { Injectable } from '@angular/core';
+
+import * as THREE from 'three';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EngineService {
+
+  constructor() {
+    // let three = AmbientLight
+  }
+}
